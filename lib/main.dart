dc1f6bf64bdbbf48768746c0c30eea938bc91bc5
Malte Nottmeyer <nottmey@users.noreply.github.com>
2025-09-08 12:48:43 +0200

    use anonymous context (#1)

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
   await Lokalise.init(projectId: 'any, not used', sdkToken: 'any, not used');
 
   // using empty context, as we don't have user info at app start yet
-  final emptyContext = LDContextBuilder().build();
+  final emptyContext = LDContextBuilder().kind('user').anonymous(true).build();
   final ldClient = LDClient(
     LDConfig(
       '<put-your-sdk-key-here>',
@@ -84,10 +84,15 @@ final contextProvider = Provider.autoDispose((ref) {
   final builder = LDContextBuilder();
 
   final userId = ref.watch(fakeAsyncUserIdProvider).valueOrNull;
-  if (userId != null) {
-    final appLocale = ref.watch(appLocaleProvider);
+  final appLocale = ref.watch(appLocaleProvider);
 
+  if (userId != null) {
     builder.kind('user', userId).setString('language', appLocale.languageCode);
+  } else {
+    builder
+        .kind('user')
+        .anonymous(true)
+        .setString('language', appLocale.languageCode);
   }
 
   return builder.build();
